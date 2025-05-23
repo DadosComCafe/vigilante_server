4f5d0d220e0f3b05b22c42d3cfb564530f0bfe4b
DadosComCafe <dadoscomcafe.dev@gmail.com>
2025-05-23 18:14:05 -0300

    (.): Reorganiza

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from python_sample_xlsx_report.src.python_generate_xlsx_report import add_numeric_sheet_to_file, gera_metrica
-import streamlit as st
+from typing import Any
+
+import gradio as gr
 
 
-def handle_file(file) -> st.file_uploader:
-    if file is not None:
-        content = file.getvalue()
-        return content
-    else:
-        st.write("Nenhum arquivo foi carregado.")
+def generate_report(file: Any) -> str:
+    """This function receive the file and call `add_numeric_sheet_to_file` function to generates a new 
+    xlsx file report.
+
+    Args:
+        file (Any): The input file
+
+    Returns:
+        str: The output file path
+    """
+
+    path_in :str = file.name
+    add_numeric_sheet_to_file(path_in)
+    #TODO: chamar a função gera_metrica, passando como parâmetro `path_in.replace('.xlsx', '_numeric.xlsx')`
+    #gera_metrica()
+    return path_in.replace('.xlsx', '_numeric.xlsx')
+
+with gr.Blocks() as demo:
+    file_in = gr.File(label="Envie um arquivo XLSX", file_types=['.xlsx'])
+    btn_download = gr.DownloadButton(label="Download Report")
     
+    file_in.change(fn=generate_report, inputs=file_in, outputs=btn_download)
 
-if __name__ == "__main__":
-    st.title("Upload de Arquivo com Função e Streamlit")
-    uploaded_file = st.file_uploader("Escolha um arquivo para upload")
-    if uploaded_file: 
-        file = handle_file(uploaded_file)
-        add_numeric_sheet_to_file(file)
-        gera_metrica(file)
-        #TODO: entender como manipular o arquivo
+demo.launch()
